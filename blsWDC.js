2af8b7584d3e25ea1114c3b8895a092a13eff28a
bjostad <bjostad@gmail.com>
2017-03-23 18:31:38 -0700

    Initial commit
    First commit for the working BLS WDC

---
+++ blsWDC.js
@@ -0,0 +1,91 @@
+(function () {
+    var myConnector = tableau.makeConnector();
+    var selectedData = {
+        "seriesid"  :["NoInputCollected"],
+        "registrationkey":"NoInputCollected"
+    };
+    console.log(selectedData);
+
+    myConnector.getSchema = function (schemaCallback) {
+        console.log('getSchema');
+        var cols = [
+            {id: "seriesID", alias: "seriesID", dataType: tableau.dataTypeEnum.string},
+            {id: "year", alias: "Year", dataType: tableau.dataTypeEnum.int},
+            {id: "period", alias: "Period", dataType: tableau.dataTypeEnum.string}, 
+            {id: "periodName", alias: "Month", dataType: tableau.dataTypeEnum.string}, 
+            {id: "value", alias: "value", dataType: tableau.dataTypeEnum.int}, 
+            {id: "footnotes", alias: "footnotes", dataType: tableau.dataTypeEnum.string}            
+        ];
+
+        var tableInfo = {
+            id : "BLS",
+            alias : "UnemploymentData",
+            columns : cols
+        };
+        console.log('tableInfo');
+
+        schemaCallback([tableInfo]);
+        console.log('schemaCallback');
+    };
+
+    myConnector.getData = function(table, doneCallback) {
+        console.log('getData');
+
+        selectedData.registrationkey = tableau.password;
+        console.log('Importing API Key');
+        console.log(selectedData);
+
+        selectedData.seriesid = tableau.connectionData.split(',');
+        console.log('Importing and splitting seriesID');
+        console.log(selectedData);
+
+        $.ajax({
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify(selectedData),
+            type: 'POST',
+            url: 'https://cors-anywhere.herokuapp.com/https://api.bls.gov/publicAPI/v1/timeseries/data/',
+            success: function(resp){
+                var resultSets = resp.Results,
+                    tableData = [];
+                console.log('resultSets::'+resultSets);
+                console.log(resultSets);
+
+                $.each(resultSets.series, function(index, obj) {
+                    console.log('object::');
+                    console.log(obj);
+                    console.log("length: "+obj.data.length);
+
+
+                    for (var i = 0, len = obj.data.length; i < len; i++) {
+                        tableData.push({
+                            "seriesID": obj.seriesID,
+                            "year": obj.data[i].year,
+                            "period": obj.data[i].period,
+                            "periodName": obj.data[i].periodName,
+                            "value": obj.data[i].value,
+                            "footnotes": obj.data[i].footnotes[0].text
+                        });
+                    }
+
+                })
+                console.log(tableData);
+                table.appendRows(tableData);
+                doneCallback();
+            }
+
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+    $(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "BLS Unemployment Data";
+        //Collect the seriesIDs as well as the API key and pass them using tableau.password and tableau.connectionData
+        tableau.password = document.getElementById('apiKeyInput').value;
+        tableau.connectionData = document.getElementById('seriesIDInput').value;
+
+        tableau.submit();
+    });
+});
+})();
